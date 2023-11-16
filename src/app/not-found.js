b025c91e665dbc7ad6e8a1d8d8b9e72e576cb924
Rakib Hossain <isheikhrakibhossain@gmail.com>
2023-11-16 22:56:57 +0600

    Not found page text designed

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFoundPage = () => {
     return (
-        <div>
-            I am not found
+        <div className='flex justify-center items-center text-6xl font-bold pt-10'>
+            404 | Not Found
         </div>
     );
 };
